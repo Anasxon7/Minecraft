5c88f42a001c58ced840fb3b1374b669d4203bd9
Madaminov <muhammadanasxon6694@gmail.com>
2022-10-01 02:19:03 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Player } from './components/Player'
 import { FPV } from './components/FPV'
 import { Cubes } from './components/Cubes'
 import { TextureSelector } from './components/TextureSelector';
-import { Menu } from './components/Menu';
 
 function App() {
   return (
